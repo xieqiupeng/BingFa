8faa909e2e22f57bdbde9b26126ac3ce1476d95e
xieqiupeng <xieqiupeng@gmail.com>
2020-08-19 10:09:47 +0800

    update

---
+++ Enum-BingFa/ThreadLocal/dependency.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Dependency
+ln -sf /Volumes/Project/Out/Out-BingFa/徐隆曦-并发编程/44  $Path/44
+ln -sf /Volumes/Project/Out/Out-BingFa/徐隆曦-并发编程/45  $Path/45
+ln -sf /Volumes/Project/Out/Out-BingFa/徐隆曦-并发编程/46  $Path/46
+ln -sf /Volumes/Project/Out/Out-BingFa/徐隆曦-并发编程/47  $Path/47
+
+# Root
+Path=/Volumes/Project/Enum/Enum-Technology/BingFa/Enum-BingFa/ThreadLocal
+ln -sf /Volumes/Project/Out/Out-BingFa/徐隆曦-并发编程/44  $Path/44
+ln -sf /Volumes/Project/Out/Out-BingFa/徐隆曦-并发编程/45  $Path/45
+ln -sf /Volumes/Project/Out/Out-BingFa/徐隆曦-并发编程/46  $Path/46
+ln -sf /Volumes/Project/Out/Out-BingFa/徐隆曦-并发编程/47  $Path/47
+
+
+
+
+
